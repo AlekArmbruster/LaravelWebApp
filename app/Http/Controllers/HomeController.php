95b70b0f416882c3578affbb98d596cb51d984d6
Alek Armbrustet <alekarmbrustet@Aleks-MacBook-Pro.local>
2018-02-07 10:47:20 +0100

    Dodat prijekat na git

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function __invoke(){
+        // $this->databaseInsert();
+        // $this->databaseInsert();
+        // $this->databaseUpdate();
+        // $this->databaseDelete();
+        
+        return view('home');
+    }
+
+
+    // public function databaseInsert(){
+    //     $id = DB::table('users')->insertGetId
+    //       ['email' => 'john@example.com', 
+    //       'password' => 'pass123', 
+    //       'firstName' => 'John', 
+    //       'lastName' => 'Doe', 
+    //       'company' => 'Vivify', 
+    //       'contry' => 'United States']
+    //     );
+
+    // }
+
+    // public function databaseUpdate() {
+    //     DB::table('users')
+    //         ->where('id', 1)
+    //         ->update(['firstName' => 'Jack']);
+    // }
+
+    // public function databaseDelete() {
+    //     DB::table('users')->truncate();
+    // }
+
+
+}
